c5f139620d9959cb8900ac88cda95a8bc60839d2
Josua Grawitter <josua.grawitter@campus.tu-berlin.de>
2019-07-29 19:21:17 +0200

    add initial tests

---
+++ tests/runtests.jl
@@ -0,0 +1,22 @@
+using Test
+using NumberIntervals
+
+a = NumberInterval(-1, 0)
+b = NumberInterval(-0.5, 0.5)
+c = NumberInterval(0.5, 2)
+z = NumberInterval(-0, 0)
+
+@testset "number comparison" begin
+    @test a < c
+    @test c > a
+    @test_throws IndeterminateException a < b
+    @test_throws IndeterminateException c > b
+    @test !(c < a)
+    @test !(a > c)
+end
+@testset "testing for zero" begin
+    @test !iszero(c)
+    @test iszero(z)
+    @test_throws IndeterminateException iszero(a)
+    @test_throws IndeterminateException iszero(b)
+end
