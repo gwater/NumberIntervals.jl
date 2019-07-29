480ec36dec0fb8f40b388a109fa41b6bf980316f
Josua Grawitter <josua.grawitter@campus.tu-berlin.de>
2019-07-29 23:49:58 +0200

    import min, max

--- src/basic.jl
+++ src/basic.jl
@@ -1,10 +1,10 @@
 
 import Base: -, +, *, /, ^, abs, abs2, sqrt, exp, exp2, expm1, exp10, log, log2,
     log1p, log10, sin, sinpi, cos, cospi, tan, asin, acos, atan, sinh, cosh,
-    asinh, acosh, tanh, atanh, inv, floor, ceil
+    asinh, acosh, tanh, atanh, inv, floor, ceil, min, max
 export -, +, *, /, ^, abs, abs2, sqrt, exp, exp2, expm1, exp10, log, log2,
     log1p, log10, sin, sinpi, cos, cospi, tan, asin, acos, atan, sinh, cosh,
-    asinh, acosh, tanh, atanh, inv, floor, ceil
+    asinh, acosh, tanh, atanh, inv, floor, ceil, min, max
 
 for f in (:-, :abs, :abs2, :sqrt, :exp, :exp2, :expm1, :exp10, :log, :log2,
           :log1p, :log10, :sin, :sinpi, :cos, :cospi, :tan, :asin, :acos, :atan,
@@ -12,7 +12,7 @@ for f in (:-, :abs, :abs2, :sqrt, :exp, :exp2, :expm1, :exp10, :log, :log2,
     @eval $f(a::NumberInterval) = NumberInterval($f(Interval(a)))
 end
 
-for f in (:+, :-, :*, :/, :^, :atan)
+for f in (:+, :-, :*, :/, :^, :atan, :min, :max)
     @eval $f(a::NumberInterval, b::NumberInterval) =
         NumberInterval($f(Interval(a), Interval(b)))
 end
