3138ec8996eadac5f69fd7b249a6888c31df4acb
Josua Grawitter <josua.grawitter@campus.tu-berlin.de>
2019-07-30 00:09:30 +0200

    import round

--- src/basic.jl
+++ src/basic.jl
@@ -1,14 +1,15 @@
 
 import Base: -, +, *, /, ^, abs, abs2, sqrt, exp, exp2, expm1, exp10, log, log2,
     log1p, log10, sin, sinpi, cos, cospi, tan, asin, acos, atan, sinh, cosh,
-    asinh, acosh, tanh, atanh, inv, floor, ceil, min, max
+    asinh, acosh, tanh, atanh, inv, floor, ceil, min, max, round, trunc
 export -, +, *, /, ^, abs, abs2, sqrt, exp, exp2, expm1, exp10, log, log2,
     log1p, log10, sin, sinpi, cos, cospi, tan, asin, acos, atan, sinh, cosh,
-    asinh, acosh, tanh, atanh, inv, floor, ceil, min, max
+    asinh, acosh, tanh, atanh, inv, floor, ceil, min, max, round, trunc
 
 for f in (:-, :abs, :abs2, :sqrt, :exp, :exp2, :expm1, :exp10, :log, :log2,
           :log1p, :log10, :sin, :sinpi, :cos, :cospi, :tan, :asin, :acos, :atan,
-          :sinh, :cosh, :asinh, :acosh, :tanh, :atanh, :inv, :floor, :ceil)
+          :sinh, :cosh, :asinh, :acosh, :tanh, :atanh, :inv, :floor, :ceil,
+          :round, :trunc)
     @eval $f(a::NumberInterval) = NumberInterval($f(Interval(a)))
 end
 
@@ -19,3 +20,5 @@ end
 
 ^(a::NumberInterval, n::Union{Integer, Rational, AbstractFloat}) =
     NumberInterval(Interval(a)^n)
+
+round(a::NumberInterval, mode) = NumberInterval(round(Interval(a), mode))
